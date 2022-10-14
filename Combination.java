72c24a95e08cdb601f06df866d6880a82842cb81
Sivabala-SBA <87258592+Sivabala-sba@users.noreply.github.com>
2022-10-14 22:55:37 +0530

    Add files via upload

---
+++ Combination.java
@@ -0,0 +1,301 @@
+package LittleAlchemyCombinations;
+
+class Combination {
+    static void acidrain(){
+        System.out.println("rain+smoke\nrain+smog\nrain+sickness\ncloud+smoke\ncloud+smog\ncloud+sickness\nrain+city");
+    }
+    static void air(){
+        System.out.println("Available from start\nfire+mist");
+    }
+    static void airplane(){
+        System.out.println("metal+bird\nsteel+bird\nbird+machine\nbird+steam engine\nbird+train\nbird+car\nbird+boat\nbird+steamboat\nbird+sailboat\nmetal+owl\nsteel+owl\nsteam engine+owl\nmetal+vulture\nsteel+vulture\nmetal+duck\nsteel+duck\nmetal+seagull\nsteel+seagull\nmetal+bat\nsteel+bat\nmetal+eagle\nsteel+eagle\nsteel+pigeon\nmetal+pigeon\nmetal+hummingbird\nsteel+hummingbird\nmetal+crow\nsteel+crow\nowl+machine\nvulture+machine\nduck+machine\nseagull+machine\nbat+machine\neagle+machine\npigeon+machine\nhummingbird+machine\ncrow+machine\npilot+container");
+    }
+    static void alarmclock(){
+        System.out.println("clock+sound\nclock+dawn\nclock+bell\nsound+watch\nwatch+dawn\nwatch+bell");
+    }
+    static void alchemist(){
+        System.out.println("human+gold\ngold+philosophy");
+    }
+    static void alcohol(){
+        System.out.println("time+juice\nwheat+fruit\nwheat+juice\nfruit+sun");
+    }
+    static void algae(){
+        System.out.println("water+plant\ngrass+pond\ngrass+lake\nwater+grass");
+    }
+    static void alien(){
+        System.out.println("life+space\nlife+galaxy\nlife+galaxy cluster\nlife+solar system\nlife+Mars\nlife+Venus\nlife+Mercury\nlife+Jupiter\nlife+Saturn");
+    }
+    static void allergy(){
+        System.out.println("dust+human\nhuman+pollen");
+    }
+    static void alligator(){
+        System.out.println("lizard+river\nswamp+lizard\nlizard+lake");
+    }
+    static void alpaca(){
+        System.out.println("mountain+sheep\nsheep+mountain range\nsheep+mountain goat");
+    }
+    static void ambulance(){
+        System.out.println("car+hospital\ncar+doctor");
+    }
+    static void angel(){
+        System.out.println("bird+human\nhuman+light");
+    }
+    static void angler(){
+        System.out.println("human+fishing rod\nsailor+fishing rod");
+    }
+    static void animal(){
+        System.out.println("life+land\nlife+forest\nlife+mountain\nlife+mountain range\nlife+ beach\nlife+desert");
+    }
+    static void ant(){
+        System.out.println("grass+animal\ngrass+spider");
+    }
+    static void antfarm(){
+        System.out.println("farm+ant\nant+jar\nglass+ant\nfarm+anthill\njar+anthill");
+    }
+    static void antarctica(){
+        System.out.println("desert+snow\ndesert+ice\nsnow+continent\nice+continent");
+    }
+    static void anthill(){
+        System.out.println("house+ant\nant+container\nant+hill\nearth+ant\nant+land\nant+soil");
+    }
+    static void apron(){
+        System.out.println("fabric+cook\nbaker+fabric");
+    }
+    static void aquarium(){
+        System.out.println("water+ glass\nglass+fish\nglass+puddle\nglass+pond\nfish+container\nsmall+swimming pool");
+    }
+    static void archeologist(){
+        System.out.println("human+ruins\nruins+science");
+    }
+    static void archipelago(){
+        System.out.println("island+island\nsea+island\nocean+island");
+    }
+    static void arctic(){
+        System.out.println("ocean+cold\nsea+cold");
+    }
+    static void armadillo(){
+        System.out.println("animal+armor\narmor+dog\narmor+cat");
+    }
+    static void armor(){
+        System.out.println("metal+fabric\nsteel+fabric");
+    }
+    static void arrow(){
+        System.out.println("bullet+wood\nwood+bow");
+    }
+    static void ash(){
+        System.out.println("fire+ mineral\nfire+plant\nfire+tree\nwater+campfire\ntime+campfire\nrain+campfire\nstorm+campfire\nfire+grass\nphoenix+paper\ncampfire+paper\nfire+paper\nsun+vampire\nvampire+dawn");
+    }
+    static void astronaut(){
+        System.out.println("human+ space\nhuman+spaceship\nhuman+space station\nhuman+rocket\nhuman+moon\nhuman+Mars\nhuman+solar system\nhuman+Mercury\nhuman+Jupiter\nhuman+Saturn\nhuman+Venus\nhuman+moon rover");
+    }
+    static void atmosphere(){
+        System.out.println("air+planet\nair+sky");
+    }
+    static void atomicbomb(){
+        System.out.println("energy+explosion\nexplosion+big\nexplosion+explosion");
+    }
+    static void aurora(){
+        System.out.println("electricity+atmosphere\nelectricity+Antarctica\nsky+Antarctica\nsky+electricity\nelectricity+arctic\nsky+arctic\nsun+atmosphere");
+    }
+    static void avalanche(){
+        System.out.println("earthquake+ snow\nwave+glacier\nearthquake+glacier\nearthquake+mountain\nwave+mountain\nearthquake+mountain range\nwave+mountain range\nglacier+sound\nmountain+sound\ngun+glacier\ngun+mountain\ngun+mountain range");
+    }
+    static void aviary(){
+        System.out.println("birdcage+big");
+    }
+    static void axe(){
+        System.out.println("blade+wood\ntool+wood\nstone+wood\ntool+lumberjack");
+    }
+    static void bacon(){
+        System.out.println("fire+pig\ncampfire+pig\nfire+ham\ncampfire+ham");
+    }
+    static void bacteria(){
+        System.out.println("life+primordial soup\nlife+small\nmud+life");
+    }
+    static void baker(){
+        System.out.println("human+bakery\nhuman+bread\nhuman+banana bread\nhuman+pizza\nhuman+toast\nhuman+batter\nhuman+pie\nhuman+donut\nhuman+cookie\nhuman+cookie dough\nhuman+dough");
+    }
+    static void bakery(){
+        System.out.println("house+bread\nhouse+baker\nhouse+donut\nhouse+cake\nbread+city\nbaker+city\ncity+donut\ncity+cake\nbread+village\nbaker+village\nvillage+donut\nvillage+cake\nbaker+container");
+    }
+    static void banana(){
+        System.out.println("fruit+monkey");
+    }
+    static void bananabread(){
+        System.out.println("bread+banana\ndough+banana");
+    }
+    static void bandage(){
+        System.out.println("blood+fabric\nsword+fabric\nblade+fabric");
+    }
+    static void bank(){
+        System.out.println("house+gold\nskyscraper+gold\nhouse+safe\nskyscraper+safe\nhouse+money\nskyscraper+money\ncity+gold\ncity+safe\ncity+money\nmoney+container\nhouse+vault");
+    }
+    static void barn(){
+        System.out.println("house+cow\nhouse+livestock\nhouse+hay\nhouse+farm\nfield+house\nlivestock+container\ncow+container\nhorse+container\npig+container\nhay+container\nhouse+sheep\nsheep+container\nhay bale+container\nhouse+goat\ngoat+container");
+    }
+    static void bat(){
+        System.out.println("sky+mouse\nair+mouse\natmosphere+mouse\nbird+mouse\nmouse+hummingbird");
+    }
+    static void batter(){
+        System.out.println("milk+flour\nflour+coconut milk");
+    }
+    static void battery(){
+        System.out.println("electricity+container\nenergy+container\nelectricity+mineral\nelectricity+ore");
+    }
+    static void bayonet(){
+        System.out.println("blade+gun\ngun+sword\naxe+gun");
+    }
+    static void bbq(){
+        System.out.println("metal+campfire\nsteel+campfire\ncampfire+garden\ncampfire+house\ncampfire+meat");
+    }
+    static void beach(){
+        System.out.println("ocean+sand\nsea+sand\nsand+lake\nwater+sand\nsand+wave");
+    }
+    static void beaver(){
+        System.out.println("wood+animal\nanimal+dam\nanimal+river\nanimal+stream");
+    }
+    static void bee(){
+        System.out.println("animal+flower\nanimal+garden");
+    }
+    static void beehive(){
+        System.out.println("house+bee\ntree+bee\nforest+bee\nwall+bee\nwood+bee\nbee+container");
+    }
+    static void beekeeper(){
+        System.out.println("human+beehive\nfarmer+beehive\nhuman+bee\nfarmer+bee");
+    }
+    static void beer(){
+        System.out.println("wheat+alcohol");
+    }
+    static void bell(){
+        System.out.println("metal+sound\nsteel+sound\nwood+sound\nmetal+hammer\nsteel+hammer");
+    }
+    static void bicycle(){
+        System.out.println("wheel+wheel\nwheel+chain\nwheel+machine");
+    }
+    static void big(){
+        System.out.println("universe+philosophy\ngalaxy+philosophy\ngalaxy cluster+philosophy\nsolar system+philosophy\nsun+philosophy\nplanet+philosophy");
+    }
+    static void binoculars(){
+        System.out.println("glasses+glasses\ntelescope+telescope");
+    }
+    static void bird(){
+        System.out.println("sky+ animal \\n air+ animal \\n animal+ airplane \\n sky+ egg \\n air+ egg \\n egg+ airplane \\n time+ dinosaur \\n time+ pterodactyl\n");
+    }
+    static void birdcage(){
+        System.out.println("bird+ container \\n bird+ wall \\n bird+ safe\n");
+    }
+    static void birdhouse(){
+        System.out.println("bird+ house \\n bird+ wall \\n egg+ container\n");
+    }
+    static void blackhole(){
+        System.out.println("pressure+ star \\n pressure+ sun \\n star+ darkness \\n sun+ darkness\n");
+    }
+    static void blade(){
+        System.out.println("stone+ metal \\n stone+ steel \\n metal+ rock \\n steel+ rock\n");
+    }
+    static void blender(){
+        System.out.println("glass+ blade \\n blade+ electricity \\n blade+ windmill \\n blade+ wind turbine \\n blade+ motion\n");
+    }
+    static void blizzard(){
+        System.out.println("storm+ snow \\n snow+ hurricane \\n tornado+ snow \\n wind+ snow \\n snow+ snow \\n snow+ big\n");
+    }
+    static void blood(){
+        System.out.println("human+ blade \\n sword+ warrior\n");
+    }
+    static void bloodbag(){
+        System.out.println("blood+ container \\n blood+ sack \\n blood+ bottle\n");
+    }
+    static void boat(){
+        System.out.println("water+ wood \\n ocean+ wood \\n sea+ wood \\n wood+ lake \\n wood+ river\n");
+    }
+    static void boiler(){
+        System.out.println("pressure+ metal \\n pressure+ tool \\n steam+ metal \\n steam+ tool \\n pressure+ container\n");
+    }
+    static void bone(){
+        System.out.println("time+ corpse \\n corpse+ vulture \\n corpse+ wolf \\n meat+ wolf \\n meat+ vulture\n");
+    }
+    static void bonsaitree(){
+        System.out.println("tree+ scissors \\n tree+ pottery \\n tree+ small \\n tree+ wire\n");
+    }
+    static void book(){
+        System.out.println("paper+ legend \\n wood+ paper \\n newspaper+ newspaper \\n story+ container \\n fairy tale+ container \\n legend+ container \\n idea+ container\n");
+    }
+    static void bottle(){
+        System.out.println("milk+ container \\n water+ container \\n coconut milk+ container \\n juice+ container \\n oil+ container \\n alcohol+ container \\n wine+ container \\n beer+ container \\n chocolate milk+ container \\n soda+ container \\n perfume+ container \\n liquid+ container\n");
+    }
+    static void boulder(){
+        System.out.println("big+ rock \\n small+ hill \\n rock+ rock \\n stone+ rock \\n earth+ rock \\n stone+ big\n");
+    }
+    static void bow(){
+        System.out.println("wood+ rope \\n wood+ wire\n");
+    }
+    static void box(){
+        System.out.println("pizza+ container \\n pencil+ container \\n cookie+ container \\n cereal+ container \\n donut+ container \\n cake+ container \\n crayon+ container\n");
+    }
+    static void bread(){
+        System.out.println("fire+ dough \\n dough+ warmth \\n energy+ dough\n");
+    }
+    static void brick(){
+        System.out.println("fire+ mud \\n mud+ sun \\n fire+ clay \\n clay+ sun \\n stone+ clay\n");
+    }
+    static void bridge(){
+        System.out.println("wood+ river \\n metal+ river \\n steel+ river \\n wood+ stream \\n metal+ stream \\n steel+ stream\n");
+    }
+    static void broom(){
+        System.out.println("wood+ hay\n");
+    }
+    static void bucket(){
+        System.out.println("wood+ container \\n yogurt+ container \\n river+ bottle \\n paint+ container \\n paint+ bottle \\n chicken wing+ container \\n chicken wing+ box\n");
+    }
+    static void bullet(){
+        System.out.println("gunpowder+ metal \\n gunpowder+ steel \\n gunpowder+ tool \\n gunpowder+ container\n");
+    }
+    static void bulletproofvest(){
+        System.out.println("bullet+ armor \\n gun+ armor\n");
+    }
+    static void bus(){
+        System.out.println("");
+    }
+    static void butcher(){
+        System.out.println("");
+    }
+    static void butter(){
+        System.out.println("");
+    }
+    static void butterfly(){
+        System.out.println("");
+    }
+    static void butterflynet(){
+        System.out.println("");
+    }
+    static void cablecar(){
+        System.out.println("");
+    }
+    static void cactus(){
+        System.out.println("");
+    }
+    static void cage(){
+        System.out.println("");
+    }
+    static void cake(){
+        System.out.println("");
+    }
+    static void camel(){
+        System.out.println("");
+    }
+    static void campfire(){
+        System.out.println("");
+    }
+    static void candle(){
+        System.out.println("");
+    }
+    static void candycane(){
+        System.out.println("");
+    }
+    static void cannon(){
+        System.out.println("");
+    }
+    static void canvas(){
+        System.out.println("");
+    }
+}
