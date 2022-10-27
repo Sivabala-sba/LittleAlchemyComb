88e6779e26ea06a5867d4bd374e4151f1cd9b5e2
Sivabala-SBA <87258592+Sivabala-sba@users.noreply.github.com>
2022-10-27 18:04:28 +0530

    Update K.java

--- collections/K.java
+++ collections/K.java
@@ -1,4 +1,22 @@
 package LittleAlchemyCombinations.collections;
 
-public class K {
+import java.util.Scanner;
+import LittleAlchemyCombinations.AbstractAlpha;
+import LittleAlchemyCombinations.*;
+
+public class K extends AbstractAlpha{
+    public void alpha() {
+        System.out.println("Kaiju\nKatana\nKite\nKnife\nKnight");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the word: ");
+        String k = input.nextLine();
+        switch (k) {
+            case "kaiju" -> Combination.kaiju();
+            case "katana" -> Combination.katana();
+            case "kite" -> Combination.kite();
+            case "knife" -> Combination.knife();
+            case "knight" -> Combination.knight();
+        }
+    }
 }
+
