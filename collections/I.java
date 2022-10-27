e88c4d86bd8d8c3140ed5400bb47b1a44a1f20de
Sivabala-SBA <87258592+Sivabala-sba@users.noreply.github.com>
2022-10-27 18:02:53 +0530

    Update I.java

--- collections/I.java
+++ collections/I.java
@@ -1,4 +1,28 @@
 package LittleAlchemyCombinations.collections;
 
-public class I {
+import java.util.Scanner;
+import LittleAlchemyCombinations.AbstractAlpha;
+import LittleAlchemyCombinations.*;
+
+public class I extends AbstractAlpha{
+    @Override
+    public void alpha() {
+        System.out.println("Ice\nIce Cream\nIce Cream Truck\nIce Sculpture\nIceberg\nIced Tea\nIdea\nIgloo\nInternet\nIsland\nIvy");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the word: ");
+        String i = input.nextLine();
+        switch (i){
+            case "ice" -> Combination.ice();
+            case "ice cream" -> Combination.icecream();
+            case "ice cream truck" -> Combination.icecreamtruck();
+            case "ice sculpture" -> Combination.icesculpture();
+            case "iceberg" -> Combination.iceberg();
+            case "iced tea" -> Combination.icedtea();
+            case "idea" -> Combination.idea();
+            case "igloo" -> Combination.igloo();
+            case "internet" -> Combination.internet();
+            case "island" -> Combination.island();
+            case "ivy" -> Combination.ivy();
+        }
+    }
 }
