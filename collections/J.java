c367d2348d7f24363ed8df5f3f14ab26eb2b79c4
Sivabala-SBA <87258592+Sivabala-sba@users.noreply.github.com>
2022-10-27 18:03:36 +0530

    Update J.java

--- collections/J.java
+++ collections/J.java
@@ -1,4 +1,22 @@
 package LittleAlchemyCombinations.collections;
 
-public class J {
+import java.util.Scanner;
+import LittleAlchemyCombinations.AbstractAlpha;
+import LittleAlchemyCombinations.*;
+
+public class J extends AbstractAlpha{
+    public void alpha(){
+        System.out.println("Jack-O-Lantern\nJam\nJar\nJerky\nJuice\nJupiter");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the word: ");
+        String j = input.nextLine();
+        switch (j){
+            case "jack-o-lantern" -> Combination.jackolantern();
+            case "jam" -> Combination.jam();
+            case "jar" -> Combination.jar();
+            case "jerky" -> Combination.jerky();
+            case "juice" -> Combination.juice();
+            case "jupiter" -> Combination.jupiter();
+        }
+    }
 }
